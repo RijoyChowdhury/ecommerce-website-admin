7d672304ee2fd404b44836ff4c8c826024ded394
Rijoy Chowdhury <chowdhuryrijoy@gmail.com>
2025-02-27 00:03:05 +0530

    feat(icons): adds necessary icons

---
+++ src/assets/icons/filter.jsx
@@ -0,0 +1,21 @@
+const Filter = (props) => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 14 14"
+            width="1em"
+            height="1em"
+            {...props}
+        >
+            <path
+                fill="none"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M13.5.5H.5l5 7v6l3-2v-4z"
+            ></path>
+        </svg>
+    )
+}
+
+export default Filter
---
+++ src/assets/icons/tabs.jsx
@@ -0,0 +1,18 @@
+const Tabs = (props) => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            width="1em"
+            height="1em"
+            {...props}
+        >
+            <path
+                fill="currentColor"
+                d="M3 3h8v2H3zm10 0h8v2h-8zM3 7h8v2H3zm10 0h8v2h-8zM3 11h8v2H3zm10 0h8v2h-8zM3 15h8v2H3zm10 0h8v2h-8zM3 19h8v2H3zm10 0h8v2h-8z"
+            ></path>
+        </svg>
+    )
+}
+
+export default Tabs

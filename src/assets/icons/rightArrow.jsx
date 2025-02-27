88be8ed0f4db028e94ca3e6307cfcbd8a8de5936
Rijoy Chowdhury <chowdhuryrijoy@gmail.com>
2025-02-27 11:24:57 +0530

    feat(icons): adds right arrow icon

---
+++ src/assets/icons/rightArrow.jsx
@@ -0,0 +1,22 @@
+const RightArrow = (props) => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512 512"
+            width="1em"
+            height="1em"
+            {...props}
+        >
+            <path
+                fill="none"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="48"
+                d="m268 112l144 144l-144 144m124-144H100"
+            ></path>
+        </svg>
+    )
+}
+
+export default RightArrow
